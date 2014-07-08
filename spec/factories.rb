9369afe0637c95a5f05205d0515a2eeb3f4677fb
Bojan <bojan92zm@yahoo.com>
2014-07-08 11:29:16 +0200

    Finish user signup.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name "Bojan Drljaca"
-    email  "boja00@yahoo.com"
-    password 'something'
-    password_confirmation 'something'
+    name 		"Bojan Drljaca"
+    email  		"boja00@yahoo.com"
+    password 	"something"
+    password_confirmation "something"
   end
 end
